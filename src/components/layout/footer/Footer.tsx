ca621354863bbf558c7095b65d1b014a26a087da
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-10-02 11:09:09 +0200

    feat: add responsives

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,58 @@
+
+import styles from "./footer.module.css"
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+export default function Footer(){
+
+
+    const items = ["☺︎" ,"Loopi Web", "☺︎", "Création site web"]
+    const repeated = Array(10).fill(items).flat()
+
+    return (
+        <>
+            <article className={styles.marque}>
+                <div className={styles.track}>
+                    {repeated.map((item, i) => (
+                        <p key={i}>{item}</p>
+                    ))}
+                    {repeated.map((item, i) => (
+                        <p key={i}>{item}</p>
+                    ))}
+
+                </div>
+            </article>
+            <footer className={styles.footer}>
+                <section className={styles.container}>
+                    <article className={styles.top}>
+                        <a href="/" className={styles.brand}>
+                            <Image
+                                src="/images/loopi.png"
+                                width={128}
+                                height={128}
+                                alt="Logo Loopi Web"
+                                className={styles.logo}
+                            />
+                        </a>
+
+                        <article>
+                            <ul className={styles.navList}>
+                                <li><a href="#" className={styles.navLink}>Politique de confidentialité</a></li>
+                                <li><a href="#" className={styles.navLink}>Mentions légales</a></li>
+                                <li><Link href="/contact" className={styles.navLink}>Contact</Link></li>
+                            </ul>
+                        </article>
+                    </article>
+
+                    <hr className={styles.separator} />
+
+                    <p className={styles.copy}>
+                        © 2025 <a href="/" className={styles.copyLink}>Loopi Web</a>. Tous droits réservés.
+                    </p>
+                </section>
+            </footer>
+        </>
+    )
+}
