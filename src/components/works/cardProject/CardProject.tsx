a6bc12439b04980fbe678462b4c2786f51449007
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2026-07-05 18:42:58 +0200

    feat: add hover image to project

--- src/components/works/cardProject/CardProject.tsx
+++ src/components/works/cardProject/CardProject.tsx
@@ -1,26 +1,47 @@
 "use client"
-import {ProjectProps} from "@/types/definitions"
+
+import { useState } from "react"
+import { ProjectProps } from "@/types/definitions"
 import styles from "./cardProject.module.css"
-import Image from "next/image";
-import {CircleArrowRight} from "lucide-react";
+import Image from "next/image"
 
+export default function CardProject({ dataProject }: { dataProject: ProjectProps }) {
+    const { name, image, type, synopsis, web, year } = dataProject
 
-export default function CardProject({dataProject}: {dataProject: ProjectProps}) {
+    const [isHovered, setIsHovered] = useState(false)
+    const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
 
-            const {name, image, type,  synopsis, web, year} = dataProject
-
+    const handleMouseMove = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        setMousePos({ x: e.clientX, y: e.clientY })
+    }
 
     return (
 
+        <a
+            href={web}
+        rel="noopener noreferrer"
+        target="_blank"
+        className={styles.sectionCard}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onMouseMove={handleMouseMove}
+            >
+            <article className={styles.sectionInfo}>
+            <h2 className={styles.h2}>{name}</h2>
+        <p className={styles.syno}>{synopsis}</p>
+        <p className={styles.year}>{year}</p>
+    </article>
 
-                    <a href={web} rel={"noopener noreferrer "} target={"_blank"} className={styles.sectionCard}>
-                        <article className={styles.sectionInfo}>
-                            <h2 className={styles.h2}>{name}</h2>
-                            <p className={styles.syno}>{synopsis}</p>
-                            <p className={styles.year}>{year}</p>
-                        </article>
-                    </a>
-
-
+        <article
+            className={styles.imageWrapper}
+            style={{
+                left: mousePos.x,
+                top: mousePos.y,
+                opacity: isHovered ? 1 : 0,
+            }}
+        >
+            <Image className={styles.image} src={image} alt={name} width={320} height={220} />
+        </article>
+    </a>
     )
-}
+    }
